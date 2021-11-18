950961bc1c4c8593635f029c52fde16c538426c0
twilightwanderer <88540658+twilightwanderer@users.noreply.github.com>
2021-11-18 21:53:18 +0300

    Improve the bureaucracy system (#61323)
    Adds a blank printing system.
    
    Virtually every Space Station 13 server has its own system of bureaucracy. As a rule, it is some kind of Wiki page with markup tags and a few sample forms. And that's usually the end of it. With stamps and a filing system in place, people rarely use them. This PR takes it upon itself to remedy this situation by introducing form standards into the game itself and adding the ability to print them.
    
    Co-authored-by: twilightwanderer <twilightwanderer@users.noreply.github.com>
    Co-authored-by: tralezab <40974010+tralezab@users.noreply.github.com>
    Co-authored-by: Aleksej Komarov <stylemistake@gmail.com>

--- tgui/packages/tgui/interfaces/Photocopier.jsx
+++ tgui/packages/tgui/interfaces/Photocopier.jsx
@@ -1,44 +1,65 @@
-import {
-  ProgressBar,
-  NumberInput,
-  Button,
-  Section,
-  Box,
-  Flex,
-} from 'tgui-core/components';
+import { sortBy } from 'common/collections';
 import { useBackend } from '../backend';
+import { Box, Button, Dropdown, Flex, NumberInput, ProgressBar, Section } from '../components';
 import { Window } from '../layouts';
 
-export const Photocopier = (props) => {
-  const { data } = useBackend();
-  const { isAI, has_toner, has_item } = data;
+export const Photocopier = (props, context) => {
+  const { data } = useBackend(context);
+  const {
+    isAI,
+    has_toner,
+    has_item,
+    forms_exist,
+  } = data;
 
   return (
-    <Window title="Photocopier" width={240} height={isAI ? 309 : 234}>
+    <Window
+      title="Photocopier"
+      width={320}
+      height={512}>
       <Window.Content>
         {has_toner ? (
           <Toner />
         ) : (
           <Section title="Toner">
-            <Box color="average">No inserted toner cartridge.</Box>
+            <Box color="average">
+              No inserted toner cartridge.
+            </Box>
           </Section>
+        )}
+        {forms_exist ? (
+          <Blanks />
+        ) : (
+          <Section title="Blanks">
+            <Box color="average">
+              No forms found. Please contact your system administrator.
+            </Box>
+          </Section>  
         )}
         {has_item ? (
           <Options />
         ) : (
           <Section title="Options">
-            <Box color="average">No inserted item.</Box>
+            <Box color="average">
+              No inserted item.
+            </Box>
           </Section>
         )}
-        {!!isAI && <AIOptions />}
+        {!!isAI && (
+          <AIOptions />
+        )}
       </Window.Content>
     </Window>
   );
 };
 
-const Toner = (props) => {
-  const { act, data } = useBackend();
-  const { has_toner, max_toner, current_toner } = data;
+const Toner = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    has_toner,
+    max_toner,
+    current_toner,
+  } = data;
 
   const average_toner = max_toner * 0.66;
   const bad_toner = max_toner * 0.33;
@@ -50,12 +71,10 @@ const Toner = (props) => {
         <Button
           disabled={!has_toner}
           onClick={() => act('remove_toner')}
-          icon="eject"
-        >
+          icon="eject">
           Eject
         </Button>
-      }
-    >
+      }>
       <ProgressBar
         ranges={{
           good: [average_toner, max_toner],
@@ -64,20 +83,27 @@ const Toner = (props) => {
         }}
         value={current_toner}
         minValue={0}
-        maxValue={max_toner}
-      />
+        maxValue={max_toner} />
     </Section>
   );
 };
 
-const Options = (props) => {
-  const { act, data } = useBackend();
-  const { color_mode, is_photo, num_copies, has_enough_toner } = data;
+const Options = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    color_mode,
+    is_photo,
+    num_copies,
+    has_enough_toner,
+  } = data;
 
   return (
     <Section title="Options">
       <Flex>
-        <Flex.Item mt={0.4} width={11} color="label">
+        <Flex.Item
+          mt={0.4}
+          width={11}
+          color="label">
           Make copies:
         </Flex.Item>
         <Flex.Item>
@@ -90,12 +116,9 @@ const Options = (props) => {
             minValue={1}
             maxValue={10}
             value={num_copies}
-            onDrag={(e, value) =>
-              act('set_copies', {
-                num_copies: value,
-              })
-            }
-          />
+            onDrag={(e, value) => act('set_copies', {
+              num_copies: value,
+            })} />
         </Flex.Item>
         <Flex.Item>
           <Button
@@ -103,39 +126,36 @@ const Options = (props) => {
             icon="copy"
             textAlign="center"
             disabled={!has_enough_toner}
-            onClick={() => act('make_copy')}
-          >
+            onClick={() => act('make_copy')}>
             Copy
           </Button>
         </Flex.Item>
       </Flex>
       {!!is_photo && (
         <Flex mt={0.5}>
-          <Flex.Item mr={0.4} width="50%">
+          <Flex.Item
+            mr={0.4}
+            width="50%">
             <Button
               fluid
               textAlign="center"
-              selected={color_mode === 'Greyscale'}
-              onClick={() =>
-                act('color_mode', {
-                  mode: 'Greyscale',
-                })
-              }
-            >
+              selected={color_mode === "Greyscale"}
+              onClick={() => act('color_mode', {
+                mode: "Greyscale",
+              })}>
               Greyscale
             </Button>
           </Flex.Item>
-          <Flex.Item ml={0.4} width="50%">
+          <Flex.Item
+            ml={0.4}
+            width="50%">
             <Button
               fluid
               textAlign="center"
-              selected={color_mode === 'Color'}
-              onClick={() =>
-                act('color_mode', {
-                  mode: 'Color',
-                })
-              }
-            >
+              selected={color_mode === "Color"}
+              onClick={() => act('color_mode', {
+                mode: "Color",
+              })}>
               Color
             </Button>
           </Flex.Item>
@@ -146,16 +166,67 @@ const Options = (props) => {
         textAlign="center"
         icon="reply"
         fluid
-        onClick={() => act('remove')}
-      >
+        onClick={() => act('remove')}>
         Remove item
       </Button>
     </Section>
   );
 };
 
-const AIOptions = (props) => {
-  const { act, data } = useBackend();
+const Blanks = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    blanks,
+    category,
+    has_toner,
+  } = data;
+
+  const sortedBlanks = sortBy(
+    blank => blanks.category,
+  )(blanks || []);
+
+  const categories = [];
+  for (let blank of sortedBlanks) {
+    if (!categories.includes(blank.category)) {
+      categories.push(blank.category);
+    }
+  }
+
+  const selectedCategory = category ?? categories[0];
+  const visibleBlanks = sortedBlanks.filter(blank => (
+    blank.category === selectedCategory
+  ));
+
+  return (
+    <Section title="Blanks">
+      <Dropdown
+        width="100%"
+        options={categories}
+        selected={selectedCategory}
+        onSelected={value => act("choose_category", {
+          category: value,
+        })}
+      />
+      <Box mt={0.4}>
+        {visibleBlanks.map(blank => (
+          <Button
+            key={blank.code}
+            disabled={!has_toner}
+            onClick={() => act("print_blank", {
+              name: blank.name,
+              info: blank.info,
+            })}
+          >
+            {blank.code}
+          </Button>
+        ))}
+      </Box>
+    </Section>
+  );
+};
+
+const AIOptions = (props, context) => {
+  const { act, data } = useBackend(context);
   const { can_AI_print } = data;
 
   return (
@@ -166,8 +237,7 @@ const AIOptions = (props) => {
           icon="images"
           textAlign="center"
           disabled={!can_AI_print}
-          onClick={() => act('ai_photo')}
-        >
+          onClick={() => act('ai_photo')}>
           Print photo from database
         </Button>
       </Box>
